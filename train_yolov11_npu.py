9d30918bad68f54a0277e65e4827ae73d05e150d
zhoudong-zd <57584559+zhoudong-zd@users.noreply.github.com>
2025-07-17 15:49:50 +0800

    Update train_yolov11_npu.py
    创建模型训练脚本train_yolov11_npu.py
    
    Signed-off-by: zhoudong-zd <57584559+zhoudong-zd@users.noreply.github.com>

--- train_yolov11_npu.py
+++ train_yolov11_npu.py
@@ -21,11 +21,11 @@ ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 def parse_opt(known=False):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str, default=ROOT / 'yolov8n-obb.pt', help='initial weights path')
-    parser.add_argument('--cfg', type=str, default=ROOT / 'ultralytics/cfg/models/v8/yolov8-obb.yaml', help='model.yaml path')
-    parser.add_argument('--data', type=str, default=ROOT / 'ultralytics/cfg/datasets/DOTAv1.yaml', help='dataset.yaml path')
-    parser.add_argument('--epochs', type=int, default=100)
-    parser.add_argument('--batch', type=int, default=16)
+    parser.add_argument('--weights', type=str, default=ROOT / 'yolov11n-obb.pt', help='初始权重')
+    parser.add_argument('--cfg', type=str, default=ROOT / 'ultralytics/cfg/models/11/yolo11-obb.yaml', help='model.yaml path')
+    parser.add_argument('--data', type=str, default=ROOT / 'ultralytics/cfg/datasets/coco.yaml', help='数据集配置')
+    parser.add_argument('--epochs', type=int, default=100, help='训练轮次')
+    parser.add_argument('--batch', type=int, default=16, help='批次大小')
     parser.add_argument('--data_shuffle', default=False, action="store_true")
     parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='train, val image size (pixels)')
     parser.add_argument('--device', default='', help='npu device, i.e. 0 or 0,1,2,3 or cpu')
